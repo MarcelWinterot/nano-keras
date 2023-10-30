ace6cc59e2f5dd13a4f903a3af85c325844cc7b4
Marcel Winterot <u66_marwin_waw@technischools.com>
2023-10-30 16:06:11 +0100

    Added a demo. Although not the best, it proves the code works for now

---
+++ src/demo.py
@@ -0,0 +1,60 @@
+from optimizers import Adam
+from callbacks import EarlyStopping
+from layers import Dense
+from losses import *
+from main import NN
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+X = sns.load_dataset("titanic")
+
+
+def drop_data(data: pd.DataFrame, data_to_drop: list[str]) -> pd.DataFrame:
+    for column in data_to_drop:
+        data.drop(column, axis=1, inplace=True)
+    return data
+
+
+X = drop_data(X, ["fare", "embarked", "adult_male", "who",
+                  "embark_town", "class", "alive", "deck", "alone"])
+
+y = X.pop("survived")
+
+encoder = {"male": 0, "female": 1}
+X["sex"] = X["sex"].map(encoder)
+
+
+X = X.dropna()
+y = y.loc[X.index]
+
+scaler = MinMaxScaler()
+
+X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+y_train = np.array([[y] for y in y_train])
+y_test = np.array([[y] for y in y_test])
+
+model = NN()
+model.add(Dense(5, "relu"))
+# model.add(Dense(12, "relu"))
+model.add(Dense(25, "relu"))
+model.add(Dense(10, "relu"))
+model.add(Dense(5, "relu"))
+model.add(Dense(1, "sigmoid"))
+
+optimizer = Adam()
+loss = MSE()
+
+model.compile(loss, optimizer, metrics="accuracy")
+
+model.train(X_train, y_train, 50, verbose=2, validation_data=(X_test, y_test))
--- src/losses.py
+++ src/losses.py
@@ -14,12 +14,14 @@ class Loss:
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
         pass
 
+
 class MAE(Loss):
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
         return np.mean(abs(yTrue - yPred))
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
-        return yTrue
+        return -1 if yTrue - yPred >= 0 else 1
+
 
 class MSE(Loss):
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> float:
@@ -28,25 +30,37 @@ class MSE(Loss):
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
         return -2 * (yPred - yTrue)
 
+
 class BCE(Loss):
     def __init__(self) -> None:
-        self.e = 1e-18
+        self.e = 1e-7
 
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
-        return np.negative(np.average((yTrue * log2(yPred + self.e) + (1 - yTrue) * log2(1 - yPred + self.e))))
+        yPred = np.clip(yPred, self.e, 1 - self.e)
+        result = (1 - yTrue) * ln(1 - yPred + self.e) + \
+            yTrue * ln(yPred + self.e)
+        return -np.mean(result)
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
-        return np.negative(yTrue / yPred + ((1 - yTrue) / (1 - yPred)))
+        yPred = np.clip(yPred, self.e, 1 - self.e)
+        result = yTrue * ln(yPred + self.e)
+        result += (1 - yTrue) * ln(1 - yPred + self.e)
+        return -result
+
 
 class CCE(Loss):
     def __init__(self) -> None:
-        self.e = 1e-18
+        self.e = 1e-7
 
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
-        return np.negative(np.sum(yTrue * ln(yPred + self.e)))
+        return -np.sum(yTrue * ln(yPred + self.e))
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
-        return np.negative(yTrue / (yPred + self.e))
+        # return -yTrue / (yPred + self.e)
+        yPred = np.sum(yPred, keepdims=True)
+        yPred = np.clip(yPred, self.e, 1.0-self.e)
+        return np.sum(yTrue * ln(yPred))
+
 
 class Hinge(Loss):
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
@@ -54,6 +68,7 @@ class Hinge(Loss):
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
         return -yTrue * (1 - yTrue * yPred > 0).astype(float)
+
 
 class Huber(Loss):
     def __init__(self, delta: float = 0.5) -> None:
@@ -68,8 +83,19 @@ class Huber(Loss):
         return np.where(absolute_error <= self.delta,
                         yPred - yTrue, self.delta * np.sign(yPred - yTrue))
 
+
 if __name__ == "__main__":
-    yTrue = np.array([[0], [0], [1], [1]])
-    yPred = np.array([[0], [0.5], [0.951], [1]])
+    from keras.losses import CategoricalCrossentropy
+    from keras.backend import categorical_crossentropy
+    yTrue = np.array([1., 1., 1.]).reshape(-1, 1)
+    yPred = np.array([1., 1., 0.,]).reshape(-1, 1)
+
     loss = CCE()
-    print(loss.compute_loss(yTrue, yPred))
+    cce = CategoricalCrossentropy()
+
+    print(f"My compute_loss: {loss.compute_loss(yTrue, yPred)}")
+    print(f"Keras compute loss: {cce(yTrue, yPred)}")
+    print(
+        f"My compute_derivative: {loss.compute_derivative(yTrue, yPred)}")
+    print(
+        f"keras.losses.binary_crossentropy: {categorical_crossentropy(yTrue, yPred)}")
