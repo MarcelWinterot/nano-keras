a7a17e1a236200f070c8fdc889997c6b65a2a052
MarcelWinterot <u66_marwin_waw@technischools.com>
2023-11-13 13:30:41 +0100

    Finished writting documentation of functions

--- src/losses.py
+++ src/losses.py
@@ -1,33 +1,86 @@
 import numpy as np
 
-log2 = np.log2
-ln = np.log
-
 
 class Loss:
     def __init__(self) -> None:
+        """Initalizer for all Loss classes that don't have any paramters to set
+        """
         pass
 
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Base implementation of compute_loss function
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed loss
+        """
         pass
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Base implementation of derivative of compute_loss, which computes the gradient used during backpropagation
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed gradient
+        """
         pass
 
 
 class MAE(Loss):
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Mean absolute error implementation of compute_loss function
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed loss
+        """
         return np.mean(abs(yTrue - yPred))
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Mean absolut error implementation of derivative of compute_loss, which computes the gradient used during backpropagation
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed gradient
+        """
         return -1 if yTrue - yPred >= 0 else 1
 
 
 class MSE(Loss):
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> float:
+        """Mean squared error implementation of compute_loss function
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed loss
+        """
         return np.mean(np.square(yTrue - yPred))
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Mean squared error implementation of derivative of compute_loss, which computes the gradient used during backpropagation
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed gradient
+        """
         return -2 * (yPred - yTrue)
 
 
@@ -36,15 +89,33 @@ class BCE(Loss):
         self.e = 1e-7
 
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Binary cross entropy implementation of compute_loss function
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed loss
+        """
         yPred = np.clip(yPred, self.e, 1 - self.e)
-        result = (1 - yTrue) * ln(1 - yPred + self.e) + \
-            yTrue * ln(yPred + self.e)
+        result = (1 - yTrue) * np.log(1 - yPred + self.e) + \
+            yTrue * np.log(yPred + self.e)
         return -np.mean(result)
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Binary cross entropy implementation of derivative of compute_loss, which computes the gradient used during backpropagation
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed gradient
+        """
         yPred = np.clip(yPred, self.e, 1 - self.e)
-        result = yTrue * ln(yPred + self.e)
-        result += (1 - yTrue) * ln(1 - yPred + self.e)
+        result = yTrue * np.log(yPred + self.e)
+        result += (1 - yTrue) * np.log(1 - yPred + self.e)
         return -result
 
 
@@ -53,31 +124,90 @@ class CCE(Loss):
         self.e = 1e-7
 
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
-        return -np.sum(yTrue * ln(yPred + self.e))
+        """Categorical cross entropy implementation of compute_loss function
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed loss
+        """
+        return -np.sum(yTrue * np.log(yPred + self.e))
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Categorical cross entropy implementation of derivative of compute_loss, which computes the gradient used during backpropagation
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed gradient
+        """
         yPred = np.sum(yPred, keepdims=True)
         yPred = np.clip(yPred, self.e, 1.0-self.e)
-        return np.sum(yTrue * ln(yPred))
+        return np.sum(yTrue * np.log(yPred))
 
 
 class Hinge(Loss):
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Hinge implementation of compute_loss function
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed loss
+        """
         return np.mean(np.maximum(0, 1 - yTrue * yPred))
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Hinge implementation of derivative of compute_loss, which computes the gradient used during backpropagation
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed gradient
+        """
         return -yTrue * (1 - yTrue * yPred > 0).astype(float)
 
 
 class Huber(Loss):
     def __init__(self, delta: float = 0.5) -> None:
+        """Initalizer for huber loss class
+
+        Args:
+            delta (float, optional): Determines the transition point between the quadratic and linear regions of the loss function. Defaults to 0.5.
+        """
         self.delta = delta
 
     def compute_loss(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Huber implementation of compute_loss function
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed loss
+        """
         absolute_error = np.abs(yTrue - yPred)
         return np.mean(np.where(absolute_error <= self.delta, 0.5 * (absolute_error**2), self.delta * (absolute_error - 0.5 * self.delta)))
 
     def compute_derivative(self, yTrue: np.ndarray, yPred: np.ndarray) -> np.ndarray:
+        """Huber implementation of derivative of compute_loss, which computes the gradient used during backpropagation
+
+        Args:
+            yTrue (np.ndarray): y dataset to which we compare model predictions
+            yPred (np.ndarray): model predictions on x dataset corresponding to y
+
+        Returns:
+            np.ndarray: computed gradient
+        """
         absolute_error = np.abs(yTrue - yPred)
         return np.where(absolute_error <= self.delta,
                         yPred - yTrue, self.delta * np.sign(yPred - yTrue))
