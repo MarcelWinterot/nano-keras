98dbc00245c30503643b384295af2bea5e262912
Marcel Winterot <u66_marwin_waw@technischools.com>
2023-12-26 21:17:47 +0100

    Added MHA feedforward prototype

--- nano_keras/layers/MultiHeadAttention.py
+++ nano_keras/layers/MultiHeadAttention.py
@@ -1,25 +1,24 @@
 import numpy as np
 from nano_keras.layers import Layer, LayerWithParams
-from nano_keras.activations import Activation, ACTIVATIONS
+from nano_keras.activations import ACTIVATIONS
 from nano_keras.optimizers import Optimizer
 from nano_keras.initializers import INITIALIZERS, Initializer
 
 
 class MultiHeadAttention(LayerWithParams):
-    def __init__(self, num_heads: int, key_dim: int, attention_axes: int | tuple = None, weight_initalizer: Initializer | str = "random_normal", bias_initalizer: Initializer | str = "zeros", name: str = "MultiHeadAttention") -> None:
-        self.num_heads = num_heads
-        self.key_dim = key_dim
-        self.attention_axes = attention_axes
+    def __init__(self, num_heads: int, key_dim: int, weight_initalizer: Initializer | str = "random_normal", bias_initalizer: Initializer | str = "zeros", name: str = "MultiHeadAttention") -> None:
+        self.num_heads: int = num_heads
+        self.key_dim: int = key_dim
 
-        self.weight_initalizer = INITIALIZERS[weight_initalizer] if type(
+        self.weight_initalizer: Initializer = INITIALIZERS[weight_initalizer] if type(
             weight_initalizer) == str else weight_initalizer
-        self.bias_initalizer = INITIALIZERS[bias_initalizer] if type(
+        self.bias_initalizer: Initializer = INITIALIZERS[bias_initalizer] if type(
             bias_initalizer) == str else bias_initalizer
 
-        self.name = name
+        self.name: str = name
 
-        self.weights = np.array([])
-        self.biases = np.array([])
+        self.weights: np.ndarray = np.array([])
+        self.biases: np.ndarray = np.array([])
 
     def output_shape(self, layers: list[Layer], current_layer_index: int) -> tuple:
         self.output_shape_value = tuple(layers[current_layer_index -
@@ -41,19 +40,16 @@ class MultiHeadAttention(LayerWithParams):
         input_shape = tuple(layers[current_layer_index -
                                    1].output_shape(layers, current_layer_index-1))
 
-        self.weights = [np.random.randn(input_shape[-1], self.num_heads, self.key_dim).astype(weight_data_type) for _ in range(3)] + \
-            [np.random.randn(self.num_heads, self.key_dim).astype(weight_data_type) for _ in range(3)] + \
-            [np.random.randn(self.num_heads, self.key_dim,
-                             input_shape[-1]).astype(weight_data_type)]
+        self.weights = self.weight_initalizer(
+            (3, *input_shape), weight_data_type)
 
-        self.biases = np.random.randn(
-            input_shape[-1]).astype(bias_data_type)
+        self.biases = self.bias_initalizer(input_shape[-1], bias_data_type)
 
         self.output_shape_value = tuple(layers[current_layer_index -
                                                1].output_shape(layers, current_layer_index-1))
 
-    def attention(self, q: np.ndarray, k: np.ndarray, v: np.ndarray) -> np.ndarray:
-        matmul_qk = np.matmul(q, k)
+    def attention(self, q: np.ndarray, k: np.ndarray, v: np.ndarray, *args, **kwargs) -> np.ndarray:
+        matmul_qk = np.matmul(q, k.T)
 
         dk = np.array(k.shape[-1])
         scaled_attention_logits = matmul_qk / np.sqrt(dk)
@@ -63,10 +59,30 @@ class MultiHeadAttention(LayerWithParams):
 
         attended_values = np.matmul(attention_weights, v)
 
+        try:
+            index = kwargs["index"]
+            attended_values += self.biases_split[index]
+        except KeyError:
+            pass
+
         return attended_values
 
     def __call__(self, x: np.ndarray, is_training: bool = False) -> np.ndarray:
-        raise NotImplementedError("MultiHeadAttention is not implemented yet")
+        Q, K, V = np.copy(x), np.copy(x), np.copy(x)
 
-    def backpropagate(self, gradient: np.ndarray, optimizer: Optimizer | list[Optimizer]) -> np.ndarray:
-        raise NotImplementedError("MultiHeadAttention is not implemented yet")
+        Q = Q * self.weights[0]
+        K = K * self.weights[1]
+        V = V * self.weights[2]
+
+        Q_split = np.array_split(Q, self.num_heads, axis=-1)
+        K_split = np.array_split(K, self.num_heads, axis=-1)
+        V_split = np.array_split(V, self.num_heads, axis=-1)
+
+        self.biases_split = np.array_split(self.biases, self.num_heads)
+
+        output = [self.attention(q, k, v, index=i) for i, (q, k, v) in enumerate(
+            zip(Q_split, K_split, V_split))]
+
+        output = np.concatenate(output, axis=-1)
+
+        return output
